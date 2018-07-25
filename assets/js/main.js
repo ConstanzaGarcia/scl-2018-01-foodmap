eb5d4806bc18329cad8b5668bf7e22777b8535f2
ConstanzaGarcia <o.conii.o@gmail.com>
2018-07-25 14:40:57 -0400

    input

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(
-            -33.4569, -70.648),
-        zoom: 8,
-    };
-    var map = new google.maps.Map(document.getElementById("mapGoogle"), mapProp);
-}
