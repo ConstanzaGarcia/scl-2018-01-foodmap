2063def3345298f87756ada3c3770d0b4f32a8af
ConstanzaGarcia <o.conii.o@gmail.com>
2018-07-26 19:54:25 -0400

    marcador en el mapa

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,79 +27,202 @@ let infowindow;
 
 
 function initMap() {
-    // The location of city
-    var pyrmont = { lat: -33.4569, lng: -70.648 };
-    // The map, centered at city
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 15, center: pyrmont });
-    // The marker, positioned at city
-    var marker = new google.maps.Marker({ position: pyrmont, map: map });
+    
+    // Ubicando mapa
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -33.4569, lng: -70.648 },
+        zoom: 15
+    });
 
+    infoWindow = new google.maps.InfoWindow;
 
+    //Ubicación especifica
 
-
-
-    // const city = { lat: -33.4569, lng: -70.648 };
-    // map = new google.maps.Map(document.getElementById('map'), {
-    //     center: pyrmont,
-    //     zoom: 15
-    // });
-
-    // let map = new google.maps.Map(
-    //     document.getElementById('map'), { zoom: 15, center: city });
-
-    infoWindow = new google.maps.InfoWindow();
-    let service = new google.maps.places.PlacesService(map);
-    service.textSearch({
-        location: pyrmont,
+    var request = {
+        location: { lat: -33.4569, lng: -70.648 },
         radius: '500',
-        type: 'restaurant',
-        query: inputSearch,
-    }, callback);
+        type: ['restaurant'],
+        key: 'AIzaSyBQearEk9C4QkT-lWKttFnXZgeonntEqaQ',
+    };
+
+    //crear service
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
 
 
-
-    const callback = (results, status) => {
+    function callback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (let i = 0; i < results.length; i++) {
-                let place = results[i];
+            for (var i = 0; i < results.length; i++) {
+                var place = results[i];
                 createMarker(results[i]);
+
+
             }
+
         }
 
+    };
+
+}
+
+function createMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position:  {lat: -33.4569, lng: -70.648},
+    });
+    marker.addListener('click', toggleBounce);
+
+
+    function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
     }
+    // marker.setMap(map);
+
+}
 
 
 
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+// // const city = { lat: -33.4569, lng: -70.648 };
+// // map = new google.maps.Map(document.getElementById('map'), {
+// //     center: pyrmont,
+// //     zoom: 15
+// // });
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-        }, function () {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
+// // let map = new google.maps.Map(
+// //     document.getElementById('map'), { zoom: 15, center: city });
 
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-    }
+// infoWindow = new google.maps.InfoWindow();
+// let service = new google.maps.places.PlacesService(map);
+// service.textSearch({
+//     location: pyrmont,
+//     radius: '500',
+//     type: ['restaurant'],
+//     query: inputSearch,
+// }, callback);
 
 
 
-};
+// const callback = (results, status) => {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//         for (let i = 0; i < results.length; i++) {
+//             let place = results[i];
+//             createMarker(results[i]);
+//         }
+//     }
+
+// }
+
+
+
+
+// // Try HTML5 geolocation.
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//         var pos = {
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude
+//         };
+
+//         infoWindow.setPosition(pos);
+//         infoWindow.setContent('Location found.');
+//         infoWindow.open(map);
+//         map.setCenter(pos);
+//     }, function () {
+//         handleLocationError(true, infoWindow, map.getCenter());
+//     });
+// } else {
+//     // Browser doesn't support Geolocation
+//     handleLocationError(false, infoWindow, map.getCenter());
+// }
+
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//     infoWindow.setPosition(pos);
+//     infoWindow.setContent(browserHasGeolocation ?
+//         'Error: The Geolocation service failed.' :
+//         'Error: Your browser doesn\'t support geolocation.');
+//     infoWindow.open(map);
+// }
+
+
+
+//         };
+
+// // // The marker, positioned at city
+// // let marker = new google.maps.Marker({ position: city, map: map });
+
+// // //Localiza nuestra posición
+
+// // // const initMap() {
+// // // map = new google.maps.Map(document.getElementById('map'), {
+// // //     center: { lat: -33.4569, lng: -70.648 },
+// // //     zoom: 15
+// // // });
+// // // infoWindow = new google.maps.InfoWindow;
+
+// // // Try HTML5 geolocation.
+// // if (navigator.geolocation) {
+// //     navigator.geolocation.getCurrentPosition(const (position) {
+// //         let pos = {
+// //             lat: position.coords.latitude,
+// //             lng: position.coords.longitude
+// //         };
+
+// //         infoWindow.setPosition(pos);
+// //         infoWindow.setContent('Location found.');
+// //         infoWindow.open(map);
+// //         map.setCenter(pos);
+// //     }, const () {
+// //         handleLocationError(true, infoWindow, map.getCenter());
+// //     });
+// // } else {
+// //     // Browser doesn't support Geolocation
+// //     handleLocationError(false, infoWindow, map.getCenter());
+// // }
+// // }
+
+// // const handleLocationError(browserHasGeolocation, infoWindow, pos) {
+// //     infoWindow.setPosition(pos);
+// //     infoWindow.setContent(browserHasGeolocation ?
+// //         'Error: The Geolocation service failed.' :
+// //         'Error: Your browser doesn\'t support geolocation.');
+// //     infoWindow.open(map);
+// // }
+
+
+// // // busca lo que queremos 
+
+// // const initialize() {
+// //     let santiago = new google.maps.LatLng(-33.4569, -70.648);
+// //     map = new google.maps.Map(document.getElementById('map'), {
+// //         center: santiago,
+// //         zoom: 15
+// //     });
+
+// //     let request = {
+// //         location: santiago,
+// //         radius: '500',
+// //         type: ['restaurant']
+// //     };
+
+// //     service = new google.maps.places.PlacesService(map);
+// //     service.nearbySearch(request, callback);
+// // }
+
+// // const callback(results, status) {
+// //     if (status == google.maps.places.PlacesServiceStatus.OK) {
+// //         for (let i = 0; i < results.length; i++) {
+// //             let place = results[i];
+// //             createMarker(results[i]);
+// //         }
+// //     }
+// // }
+
+// //         }
+
