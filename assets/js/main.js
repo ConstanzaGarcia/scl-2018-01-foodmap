0f9f7f5d70f84d3de015034a7c01efbde2f59995
ConstanzaGarcia <o.conii.o@gmail.com>
2018-07-26 18:37:32 -0400

    cambios en css y js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,19 @@ setTimeout(function hide() {
     $('#iniciando').hide('fast');
     document.getElementById('mapaPantallaUno').style.display = 'block';
 }, 3000);
-// Funciones del mapa
 
-// variables que vamos a trabajar 
+
+// Marcamos un lugar en el mapa
+
+// function initMap() {
+//     // The location of city
+//     var city = { lat: -33.4569, lng: -70.648 };
+//     // The map, centered at city
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), { zoom: 15, center: city });
+//     // The marker, positioned at city
+//     var marker = new google.maps.Marker({ position: city, map: map });
+// }
 
 
 let map;
@@ -16,109 +26,80 @@ let infowindow;
 //Iniciando Mapa
 
 
-const initMap = () => {
-    //localizando Santiago
-    const city = { lat: -33.4569, lng: -70.648 };
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: santiago,
-        zoom: 15
-    });
+function initMap() {
+    // The location of city
+    var pyrmont = { lat: -33.4569, lng: -70.648 };
+    // The map, centered at city
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 15, center: pyrmont });
+    // The marker, positioned at city
+    var marker = new google.maps.Marker({ position: pyrmont, map: map });
+
+
+
+
+
+    // const city = { lat: -33.4569, lng: -70.648 };
+    // map = new google.maps.Map(document.getElementById('map'), {
+    //     center: pyrmont,
+    //     zoom: 15
+    // });
 
     // let map = new google.maps.Map(
     //     document.getElementById('map'), { zoom: 15, center: city });
-    infoWindow = new google.maps.InfoWindow();
 
+    infoWindow = new google.maps.InfoWindow();
     let service = new google.maps.places.PlacesService(map);
     service.textSearch({
-        location: santiago,
+        location: pyrmont,
         radius: '500',
-        type: ['restaurant'],
+        type: 'restaurant',
         query: inputSearch,
     }, callback);
 
 
 
     const callback = (results, status) => {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
             for (let i = 0; i < results.length; i++) {
                 let place = results[i];
                 createMarker(results[i]);
-
             }
         }
 
     }
 
+
+
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+    }
+
+
+
 };
-
-// // The marker, positioned at city
-// let marker = new google.maps.Marker({ position: city, map: map });
-
-// //Localiza nuestra posición
-
-// // const initMap() {
-// // map = new google.maps.Map(document.getElementById('map'), {
-// //     center: { lat: -33.4569, lng: -70.648 },
-// //     zoom: 15
-// // });
-// // infoWindow = new google.maps.InfoWindow;
-
-// // Try HTML5 geolocation.
-// if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(const (position) {
-//         let pos = {
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude
-//         };
-
-//         infoWindow.setPosition(pos);
-//         infoWindow.setContent('Location found.');
-//         infoWindow.open(map);
-//         map.setCenter(pos);
-//     }, const () {
-//         handleLocationError(true, infoWindow, map.getCenter());
-//     });
-// } else {
-//     // Browser doesn't support Geolocation
-//     handleLocationError(false, infoWindow, map.getCenter());
-// }
-// }
-
-// const handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//     infoWindow.setPosition(pos);
-//     infoWindow.setContent(browserHasGeolocation ?
-//         'Error: The Geolocation service failed.' :
-//         'Error: Your browser doesn\'t support geolocation.');
-//     infoWindow.open(map);
-// }
-
-
-// // busca lo que queremos 
-
-// const initialize() {
-//     let santiago = new google.maps.LatLng(-33.4569, -70.648);
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: santiago,
-//         zoom: 15
-//     });
-
-//     let request = {
-//         location: santiago,
-//         radius: '500',
-//         type: ['restaurant']
-//     };
-
-//     service = new google.maps.places.PlacesService(map);
-//     service.nearbySearch(request, callback);
-// }
-
-// const callback(results, status) {
-//     if (status == google.maps.places.PlacesServiceStatus.OK) {
-//         for (let i = 0; i < results.length; i++) {
-//             let place = results[i];
-//             createMarker(results[i]);
-//         }
-//     }
-// }
-
-//         }
